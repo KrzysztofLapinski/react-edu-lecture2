b601498f1ee2b1ecee16cc00c9b3029271ef0893
klanetcompany <kla@netcompany.com>
2022-09-10 17:26:04 +0200

    sample inut

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+function SampleInput(props) {
+  return (
+    <div>
+      <label>{props.label}</label><br />
+      <input type="text" id={props.fieldName} name={props.fieldName}></input>
+      <hr />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div style={{width: "400px"}}>
-    <h1>Some form</h1>
-    <div>
-      <label>First name:</label><br/>
-      <input type="text" id="fname" name="fname" />
-      <hr/>
+    <div style={{ width: "200px" }}>
+      <h1>Some form</h1>
+      <SampleInput fieldName="fname" label="First name:" />
+      <SampleInput fieldName="lname" label="Last name:" />
+      <SampleInput fieldName="phoneNumber" label="Phone number:" />
     </div>
-    <div>
-      <label for="lname">Last name:</label><br/>
-      <input type="text" id="lname" name="lname" />
-      <hr/>
-    </div>
-    <div>
-      <label>Phone number:</label><br/>
-      <input type="text" id="phoneNumber" name="phoneNumber" />
-      <hr/>
-    </div>
-  </div>
   );
 }
 
