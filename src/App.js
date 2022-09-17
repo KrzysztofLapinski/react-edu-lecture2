a0cb1dc839a80138242b6f819afad420c018fe73
klanetcompany <kla@netcompany.com>
2022-09-17 23:13:42 +0200

    users with button to increase age - callbacks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
+function IncreaseAgeButton(props) {
+  return (
+    <button className="btn-primary" onClick={(e) => props.onClick(e, 2)}>
+      {props.children}
+    </button>
+  )
+}
+
 function User(props) {
+  function onIncreaseButtonClicked(e, noOfYears) {
+    console.log('Age was increased by ' + noOfYears + ' years');
+  }
+
   return (
     <div>
       <span>Name is: {props.name} </span>
-      <span>Age is: {props.age}</span>
+      <span>Age is: {props.age} </span>
+      <IncreaseAgeButton onClick={onIncreaseButtonClicked}>
+        Increase age
+      </IncreaseAgeButton>
     </div>
   );
 }
