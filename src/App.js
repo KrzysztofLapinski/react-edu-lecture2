d802f496c03c81dd83800b6727e1c33b1c640bd2
klanetcompany <kla@netcompany.com>
2022-09-22 20:38:43 +0200

    users with state - function component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
 import './App.css';
-import React, {Component} from 'react'
+import { useState } from 'react'
 
 function IncreaseAgeButton(props) {
   return (
-      <button className="btn-primary" onClick={(e) => props.onClick(e, 2)}>
-          {props.children}
-      </button>
+    <button className="btn-primary" onClick={(e) => props.onClick(e, 2)}>
+      {props.children}
+    </button>
   )
 }
 
-class User extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = { age: props.age };
-      // This binding is necessary to make `this` work in the callback
-      this.onIncreaseButtonClicked = this.onIncreaseButtonClicked.bind(this);
-  }
+function User(props) {
+  const [age, setAge] = useState(props.age);
 
-  onIncreaseButtonClicked(e, noOfYears) {
+  function onIncreaseButtonClicked(e, noOfYears) {
     console.log('onIncreaseButtonClicked', noOfYears);
-    //console.log('before', this.state.age);
-      this.setState({
-          age: this.state.age + noOfYears
-      });
-    //console.log('after', this.state.age);
+    setAge(age + noOfYears);
   }
-  
-  render() { 
-    console.log('User start', this.state);
 
-      return (
-          <div>
-              <span>Name is: {this.props.name}. </span>
-              <span>Age is: {this.state.age} </span>
-              <IncreaseAgeButton onClick={this.onIncreaseButtonClicked}>
-                  Increase age
-              </IncreaseAgeButton>
-          </div>
-      );
-  };
+  console.log('User start', age);
+
+  return (
+    <div>
+      <span>Name is: {props.name}. </span>
+      <span>Age is: {age} </span>
+      <IncreaseAgeButton onClick={onIncreaseButtonClicked}>
+        Increase age
+      </IncreaseAgeButton>
+    </div>
+  );
 }
 
-class App extends React.Component {
-  render() {
-    console.log('App start', this.state);
-      return (
-          <div >
-              <User name="Jan" age={25}/>
-              <User name="Anna" age={42} />
-          </div>
-      );
-  }
+function App() {
+  console.log('App start');
+  return (
+    <div >
+      <User name="Jan" age={25} />
+      <User name="Anna" age={42} />
+    </div>
+  );
 }
 
 export default App;
